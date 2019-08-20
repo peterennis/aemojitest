fd936e22da5fd6f188a21a0962efbc8fb36a5159
peterennis <peterennis@yahoo.com>
2019-08-19 18:58:56 -0700

    feat(easy): emojitest easy working

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+// import { emojisplosion, emojisplosions } from 'emojisplosion';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // Just one explosion, please.
+    //emojisplosion();
+
+    // Commence explosions!
+    //emojisplosions();
+  }
+}
