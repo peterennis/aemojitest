0cce5c7b9c56c135f397ece1287e990c8124ae23
peterennis <peterennis@yahoo.com>
2019-09-30 15:42:35 -0700

    feat(timer): 10 sec countdown timer for sploding

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,7 +14,8 @@ export class HomePage implements OnInit {
     // Just one explosion, please.
     emojisplosion();
 
-    const countDownDate = new Date('Sep 30, 2019 15:00:00').getTime();
+    let countDownDate = new Date();
+    countDownDate = new Date(countDownDate.getTime() + 1000 * 10);
 
     // Update the count down every 1 second
     const x = setInterval(() => {
@@ -23,7 +24,7 @@ export class HomePage implements OnInit {
       const now = new Date().getTime();
 
       // Find the distance between now and the count down date
-      const distance = countDownDate - now;
+      const distance = +countDownDate - now;
       // Time calculations for days, hours, minutes and seconds
       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -32,13 +33,13 @@ export class HomePage implements OnInit {
       console.log(now, 'now', 'countDownDate', countDownDate, 'distance', distance, 'days', days);
 
       // Output the result in an element with id="demo"
-      document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h '
+      document.getElementById('demo').innerHTML = '&nbsp;&nbsp;&nbsp;SPLODING will commence in ' + days + 'd ' + hours + 'h '
         + minutes + 'm ' + seconds + 's ';
 
       // If the count down is over, write some text
       if (distance < 0) {
         clearInterval(x);
-        document.getElementById('demo').innerHTML = 'EXPIRED';
+        document.getElementById('demo').innerHTML = '&nbsp;&nbsp;&nbsp;SPLODING NOW !!!';
         // Commence explosions!
         emojisplosions();
       }
